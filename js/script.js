494dcf117244db88577f51c6255c1017086eb545
muchhave <muckhave@gmail.com>
2018-08-31 23:31:12 +0900

    header and btn create

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-//test
+$(function(){
+    //ハンバーガー
+    $('.header__hbgnav').on('click',function(){
+        $('body').toggleClass('is_hbgOpen');
+    });
+    
+    //langage
+    var $lang = $('.header__lang'),
+        $langListItem = $lang.find('.header__lang__listItem'),
+        $langCrnt = $lang.find('.header__lang__crnt');
+    $lang.on('click',function(){
+        $(this).toggleClass('is_langMenuOpen');
+        checkCrntMenu();
+    });
+    $langListItem.on('click',function(){
+        $langCrnt.text($(this).text());
+    });
+    function checkCrntMenu(){
+        $langListItem.each(function(){
+            $(this).removeClass('is_crnt');
+            if($(this).text() == $langCrnt.text()){
+                $(this).addClass('is_crnt');
+            }
+        });
+    }
+    
+    //listenmode
+    $('.btnArea__btn').on('click',function(){
+        $('body').toggleClass('is_lesson');
+    });
+})
